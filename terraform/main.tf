3481fe6ca4dc7286d1d91c17f1a4c8b0394fa253
Sam <barrett370@gmail.com>
2019-09-28 15:05:11 +0100

    fucking startup scripts aren't running

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,18 @@
 provider "google" {
   credentials = file("../creds/cloudKeys.json")
   project = "bot-host-253711"
-  region  = "europe-west2"
-  zone    = "europe-west2-a"
+  region = "europe-west2"
+  zone = "europe-west2-a"
 }
 
 data "google_compute_image" "search" {
-  family  = "cos-stable"
+  family = "cos-stable"
   project = "gce-uefi-images"
 
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "ricardo-instance"
+  name = "ricardo-instance"
   machine_type = "f1-micro"
 
   boot_disk {
@@ -20,9 +20,16 @@ resource "google_compute_instance" "vm_instance" {
       image = data.google_compute_image.search.self_link
     }
   }
-  metadata_startup_script = file("./provisioning.sh")
+  metadata = {
+    sshKeys = "barrett370:${file("~/.ssh/id_rsa.pub")}"
+
+    startup-script = <<SCRIPT
+            docker login -u "gitlab-gcloud-deployment" -p "yK6Z8hU_pQTyshes5avX" registry.gitlab.com
+            docker run registry.gitlab.com/chasbob/ricardo-bot:latest
+        SCRIPT
+
+  }
   network_interface {
-    # A default network is created for all GCP projects
     network = "default"
     access_config {
     }
--- terraform/provisioning.sh
+++ terraform/provisioning.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 docker login -u "gitlab-gcloud-deployment" -p "yK6Z8hU_pQTyshes5avX" registry.gitlab.com
-docker login docker run registry.gitlab.com/chasbob/ricardo-bot:latest
+docker run registry.gitlab.com/chasbob/ricardo-bot:latest
