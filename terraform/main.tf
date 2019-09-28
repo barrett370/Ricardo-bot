3a249b2c9bfcdc63ca609144325d441d0366394a
Sam <barrett370@gmail.com>
2019-09-28 12:18:51 +0100

    explicit base image name

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,19 +5,13 @@ provider "google" {
   zone    = "europe-west2-a"
 }
 
-data "google_compute_image" "search" {
-  family  = "cos-stable"
-  project = "gce-uefi-images"
-
-}
-
 resource "google_compute_instance" "vm_instance" {
   name         = "ricardo-instance"
   machine_type = "f1-micro"
 
   boot_disk {
     initialize_params {
-      image = data.google_compute_image.search.self_link
+      image = "gce-uefi-images/family/cos-stable"
     }
   }
   metadata_startup_script = "docker run registry.gitlab.com/chasbob/ricardo-bot:latest"
