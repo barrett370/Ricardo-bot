55e346d0c7a2dbadcb7a2122833e3a28772da1e8
Sam <barrett370@gmail.com>
2019-10-02 23:38:05 +0100

    added restart policy

--- terraform/provisioning.sh
+++ terraform/provisioning.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo useradd foo && sudo usermod -a -G docker foo
 sudo -u foo bash -c 'echo yK6Z8hU_pQTyshes5avX | docker login -u gitlab-gcloud-deployment --password-stdin registry.gitlab.com'
-sudo -u foo bash -c 'docker run registry.gitlab.com/chasbob/ricardo-bot:latest'
+sudo -u foo bash -c 'docker run  --restart unless-stopped registry.gitlab.com/chasbob/ricardo-bot:latest'
