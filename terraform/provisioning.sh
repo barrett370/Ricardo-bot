643d06ead9e471b0bb948aa3018bd9b3ff9682da
Sam <barrett370@gmail.com>
2019-10-02 23:24:07 +0100

    she works!

--- terraform/provisioning.sh
+++ terraform/provisioning.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir /var/lib/ricardo
 cd /var/lib/ricardo || exit 94
-echo yK6Z8hU_pQTyshes5avX | docker login -u gitlab-gcloud-deployment --password-stdin registry.gitlab.com &>> /dev/null|| exit 95
-docker run registry.gitlab.com/chasbob/ricardo-bot:latest &>> /dev/null|| exit 96
+sudo useradd foo && sudo usermod -a -G docker foo
+sudo -u foo bash -c 'echo yK6Z8hU_pQTyshes5avX | docker login -u gitlab-gcloud-deployment --password-stdin registry.gitlab.com' &>> /dev/null|| exit 95
+sudo -u foo bash -c 'docker run registry.gitlab.com/chasbob/ricardo-bot:latest &>> /dev/null|| exit 96'
