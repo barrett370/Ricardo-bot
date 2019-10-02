b0ab78794a91bf6c791210576fc4ba6e6e2fc945
Sam Barrett <barrett370@gmail.com>
2019-10-02 23:30:41 +0100

    Fix CI deployment (#15)
    * WIP
    
    * she works!
    
    * tidy

--- terraform/provisioning.sh
+++ terraform/provisioning.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-docker login -u "gitlab-gcloud-deployment" -p "yK6Z8hU_pQTyshes5avX" registry.gitlab.com
-docker run registry.gitlab.com/chasbob/ricardo-bot:latest
+sudo useradd foo && sudo usermod -a -G docker foo
+sudo -u foo bash -c 'echo yK6Z8hU_pQTyshes5avX | docker login -u gitlab-gcloud-deployment --password-stdin registry.gitlab.com'
+sudo -u foo bash -c 'docker run registry.gitlab.com/chasbob/ricardo-bot:latest'
