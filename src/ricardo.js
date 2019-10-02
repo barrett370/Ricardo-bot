60c55c021d9911e4d07babc708027bc2d8026af4
Sam <barrett370@gmail.com>
2019-10-02 15:03:11 +0100

    Auto stash before rebase of "origin/master"

--- src/ricardo.js
+++ src/ricardo.js
@@ -6,53 +6,54 @@ client.on('ready', () => {
 })
 ;
 client.on('message', msg => {
-        let pref;
-        if (msg.content.substring(0, 1) === '!') {
-            var args = msg.content.substring(1).split(' ');
-            var cmd = args[0];
+    let pref;
+    if (msg.content.substring(0, 1) === '!') {
+        var args = msg.content.substring(1).split(' ');
+        var cmd = args[0];
 
-            switch (cmd) {
-                case 'ping':
-                    msg.reply("pong");
-                    break;
-                case 'bloomberg':
-                    msg.reply("bloooooombeeerrg");
-                    break;
-                case 'bigbrain':
-                    msg.reply({
-                        files:
-                            ["./resources/bigbrain.gif"]
-                    });
-                    break;
-                case 'dad':
-                    const https = require('https');
-                    const options = {
-                        hostname: 'icanhazdadjoke.com',
-                        port: "443",
-                        path: '/',
-                        headers: {
-                            'Accept': 'application/json'
-                        }
+        switch (cmd) {
+            case 'ping':
+                msg.reply("pong");
+                break;
+            case 'bloomberg':
+                msg.reply("bloooooombeeerrg");
+                break;
+            case 'bigbrain':
+                msg.reply({
+                    files:
+                        ["./resources/bigbrain.gif"]
+                });
+                break;
+            case 'dad':
+                const https = require('https');
+                const options = {
+                    hostname: 'icanhazdadjoke.com',
+                    port: "443",
+                    path: '/',
+                    headers: {
+                        'Accept': 'application/json'
                     }
+                };
 
-                    const req = https.request(options, res => {
-                        console.log(`statusCode: ${res.statusCode}`)
+                const req = https.request(options, res => {
+                    console.log(`statusCode: ${res.statusCode}`);
 
                     res.on('data', d => {
                         var jsonContent = JSON.parse(d);
-                        msg.reply(jsonContent.joke)
+                        msg.reply(jsonContent.joke);
                         process.stdout.write(d)
-                    })
+                    });
 
                     req.on('error', error => {
                         console.error(error)
                         msg.reply("piss off");
-                    })
+                    });
 
                     req.end()
 
 
-            }
+                });
+
         }
         if (((msg.content.toLowerCase().includes("i'm")) || (msg.content.toLowerCase().includes("i am"))) && msg.author.tag !== client.user.tag) {
 
@@ -79,7 +80,7 @@ client.on('message', msg => {
             let reply = "Hi" + iam + ", I'm Dad!";
             msg.reply(reply)
         }
-        if(msg.content.toLowerCase().includes("ricardo")){
+        if (msg.content.toLowerCase().includes("ricardo")) {
             msg.reply({
                 files:
                     ["./resources/ricardo.gif"]
