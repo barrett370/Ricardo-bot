f3689983c7abe6f8d206af8805040366be65822d
Sam <barrett370@gmail.com>
2019-10-03 17:33:27 +0100

    revert debug tag change

--- terraform/provisioning.sh
+++ terraform/provisioning.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo useradd foo && sudo usermod -a -G docker foo
 echo "$PASSWORD" | sudo -u foo bash -c 'docker login -u gitlab-gcloud-deployment --password-stdin registry.gitlab.com'
-sudo -u foo bash -c 'docker run  --restart unless-stopped registry.gitlab.com/chasbob/ricardo-bot:add_avatar'
+sudo -u foo bash -c 'docker run  --restart unless-stopped registry.gitlab.com/chasbob/ricardo-bot:latest'
