e6ad0f83c10119370c39b20eb74ffe46963e89b9
Sam <barrett370@gmail.com>
2019-10-18 14:59:51 +0100

    fix password templating

--- terraform/provisioning.sh
+++ terraform/provisioning.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo useradd foo && sudo usermod -a -G docker foo
-echo "$PASSWORD" | sudo -u foo bash -c 'docker login -u gitlab-gcloud-deployment --password-stdin registry.gitlab.com'
+echo "${PASSWORD}" | sudo -u foo bash -c 'docker login -u gitlab-gcloud-deployment --password-stdin registry.gitlab.com'
 sudo -u foo bash -c 'docker run  --restart unless-stopped registry.gitlab.com/chasbob/ricardo-bot:latest'
