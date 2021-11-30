048446b68c1fd4049a80870c9bc01083f89a190e
sbarrett <barrett370@gmail.com>
2021-11-30 20:02:43 +0000

    tidy

--- scripts/docker-make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-image_tag="$(git rev-parse --short HEAD)-$(date +%s)"
-sudo docker build -t ricardo-bot .
-sudo docker tag ricardo-bot barrett370/ricardo-bot:${image_tag}
-sudo docker push barrett370/ricardo-bot:${image_tag}
-echo "Pushed new Docker image with tag ${image_tag}"
